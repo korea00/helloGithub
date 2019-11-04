1cc915d5c44f53dae3b519053dcb5ae584c37d30
korea00 <qilil0@naver.com>
2019-11-04 10:55:33 +0900

    pro05

---
+++ pro05/src/sec01/ex01/ThirdServlet.java
@@ -0,0 +1,38 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ */
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	public void init() throws ServletException
+	{
+		System.out.println("init 메서드 호출 >>>>>");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThridServlet doGet 메서드 호출 >>>>>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThridServlet destroy 메서드 호출 >>>>>");
+	}
+
+}
